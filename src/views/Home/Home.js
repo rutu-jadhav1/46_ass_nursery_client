d6121c9e5514d74a3fa9cca4d896578c1d91be6c
rutu-jadhav1 <0103rutujajadhav@gmail.com>
2024-07-17 14:31:54 +0530

    local link added

--- src/views/Home/Home.js
+++ src/views/Home/Home.js
@@ -10,7 +10,7 @@ function Home() {
 
   const loadPlants = async () =>{
     toast.loading("Loading plants....")
-    const response = await axios.get(`https://four6-ass-nursery-server.onrender.com/plants`)
+    const response = await axios.get(`${process.env.REACT_APP_API_URL}/plants`)
     toast.dismiss()
     toast.success("Plants loaded successfully")
     setPlants(response.data.data);
