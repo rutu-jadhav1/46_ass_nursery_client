8b783a23a33364c993dcf0655a2b47ace1aec24f
rutu-jadhav1 <0103rutujajadhav@gmail.com>
2024-07-17 21:35:45 +0530

    edit and delete btn

--- src/components/PlantCard/PlantCard.js
+++ src/components/PlantCard/PlantCard.js
@@ -8,7 +8,11 @@ function PlantCard({_id, name, category, image, price, description}) {
         <p className='plant-info'>Price : {price}</p>
         <p className='plant-info'>Category : {category}</p>
         <p className='plant-info'>Description : {description}</p>
-        <img src={image} className='plant-card-image'/>
+        <img src={image} className='plant-card-image' alt='plant-img'/>
+        <div>
+          <button type='button' className='plant-card-action-btn'>Edit</button>
+          <button type='button' className='plant-card-action-btn'>Delete</button>
+        </div>
     </div>
   )
 }
